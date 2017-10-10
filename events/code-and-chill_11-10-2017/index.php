df00d7a9afcc6be9bf1ec200e1017ab519056130
wjbaker101 <willbaker10198@gmail.com>
2017-10-10 23:30:16 +0100

    Section resources for beginners and advanced topics.

--- events/code-and-chill_11-10-2017/index.php
+++ events/code-and-chill_11-10-2017/index.php
@@ -40,6 +40,7 @@
             <div class="content-width hpadding-small">
                 <section class="section text-centered">
                     <h1 class="red-heading">Code and Chill</h1>
+                    <p class="text-centered"><strong>Wednesday 11th October 2017</strong></p>
                     <p>It's our first official session of the year and it's FREE! Come and see what the society has to offer and learn something new. We will simply be coding in a room with Netflix/Music in the background.</p>
                     <p><strong>Please bring your own laptops and devices as there are no computers available at this venue!</strong></p>
                 </section>
@@ -49,24 +50,25 @@
                             <h2>Beginners Topic</h2>
                             <p>HTML and CSS are markup languages and the building blocks that make up the web.</p>
                             <p>In this topic, you will learn how to structure HTML and style it with CSS.</p>
+                            <h2>Resources:</h2>
+                            <ul class="text-left">
+                                <li><a class="page-link-underline" href="https://www.w3schools.com/html/" target="_blank">w3schools HTML Reference</a></li>
+                                <li><a class="page-link-underline" href="https://www.codecademy.com/learn/make-a-website" target="_blank">Codecademy Make-a-Website Course</a></li>
+                                <li><a class="page-link-underline" href="https://www.codecademy.com/catalog/language/html-css" target="_blank">Codecademy HTML and CSS Courses</a></li>
+                            </ul>
                         </div>
                         <div class="advanced-content cell l6 m12 hpadding-small vpadding-regular text-centered">
                             <h2>Advanced Topic</h2>
                             <p>If you've done HTML and CSS before, try out the advanced topic.</p>
                             <p>We will be going through the first steps of using SASS.</p>
                             <p>SASS is a preprocessor for writing CSS. It makes it easier to read and write CSS.</p>
+                            <h2>Resources:</h2>
+                            <ul class="text-left">
+                                <li><a class="page-link-underline" href="http://sass-lang.com/" target="_blank">SASS Website</a></li>
+                                <li><a class="page-link-underline" href="http://sass-lang.com/documentation/file.SASS_REFERENCE.html" target="_blank">SASS Documentation</a></li>
+                            </ul>
                         </div>
                     </div>
-                </section>
-                <section class="section vpadding-regular">
-                    <h2>Helpful Resources and Links:</h2>
-                    <ul>
-                        <li><a class="page-link-underline" href="https://www.w3schools.com/html/" target="_blank">w3schools HTML Reference</a></li>
-                        <li><a class="page-link-underline" href="https://www.codecademy.com/learn/make-a-website" target="_blank">Codecademy Make-a-Website Course</a></li>
-                        <li><a class="page-link-underline" href="https://www.codecademy.com/catalog/language/html-css" target="_blank">Codecademy HTML and CSS Courses</a></li>
-                        <li><a class="page-link-underline" href="http://sass-lang.com/" target="_blank">SASS Website</a></li>
-                        <li><a class="page-link-underline" href="http://sass-lang.com/documentation/file.SASS_REFERENCE.html" target="_blank">SASS Documentation</a></li>
-                    </ul>
                 </section>
             </div>
         </div>
