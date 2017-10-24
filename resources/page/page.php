e5277de492008fdbfdf9aac88097d6e00e6ca07b
wjbaker101 <willbaker10198@gmail.com>
2017-10-24 10:18:07 +0100

    Hide code and chill page.

--- resources/page/page.php
+++ resources/page/page.php
@@ -63,7 +63,7 @@ function displayHeader()
     }
     
     $links .= "<a href='https://lsu.co.uk/elections/vote/811' class='page-link-underline' target='_blank' rel='nofollow'>vote!</a>";
-    $links .= "<a style='color:#e41f28' href='/codeandchill/' class='page-link-underline'>code and chill!</a>";
+    //$links .= "<a style='color:#e41f28' href='/codeandchill/' class='page-link-underline'>code and chill!</a>";
     
     $html = <<<EOT
         <header role="header" class="hpadding-small vpadding-mid bg-white">
