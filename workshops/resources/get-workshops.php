cf39bdcbc424663a25b605529bd1c406b0662664
wjbaker101 <willbaker10198@gmail.com>
2017-12-29 13:06:36 +0000

    Edit Workshop Date Format

--- workshops/resources/get-workshops.php
+++ workshops/resources/get-workshops.php
@@ -2,17 +2,23 @@
 
 function createWorkshop($title, $date, $alias, $image)
 {
-    return (object)['title' => $title, 'eventDate' => $date, 'alias' => $alias, 'image' => $image];
+    return (object)
+    [
+        'title' => $title,
+        'eventDate' => new DateTime($date),
+        'alias' => $alias,
+        'image' => $image,
+    ];
 }
 
 $workshops = array
 (
-    createWorkshop('Hacking with Amazon Alexa', '21/10/2017', 'amazon-alexa', 'amazon-alexa.png'),
-    createWorkshop('Building Scalable Apps with CockroachDB', '22/11/2017', 'cockroachdb', 'cockroachdb.png'),
-    createWorkshop('An introduction to Git and GitHub', '02/12/2017', 'github', 'github.jpg'),
-    createWorkshop('Building an Android App', '02/12/2017', 'android', 'android.png'),
-    createWorkshop('Introduction to Docker', '02/12/2017', 'docker', 'docker.png'),
-    createWorkshop('Coursework Crash Course', '02/12/2017', 'coursework-crash-course', 'html5.svg'),
+    createWorkshop('Hacking with Amazon Alexa', '2017-10-21 16:00:00', 'amazon-alexa', 'amazon-alexa.png'),
+    createWorkshop('Building Scalable Apps with CockroachDB', '2017-11-22 16:00:00', 'cockroachdb', 'cockroachdb.png'),
+    createWorkshop('An introduction to Git and GitHub', '2017-12-02 16:00:00', 'github', 'github.jpg'),
+    createWorkshop('Building an Android App', '2017-10-21 16:00:00', 'android', 'android.png'),
+    createWorkshop('Introduction to Docker', '2017-10-21 16:00:00', 'docker', 'docker.png'),
+    createWorkshop('Coursework Crash Course', '2017-10-21 16:00:00', 'coursework-crash-course', 'html5.svg'),
 );
 
 $count = 0;
