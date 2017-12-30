1079598f05cb57ef44673721896681745b2c4379
wjbaker101 <willbaker10198@gmail.com>
2017-12-30 21:32:51 +0000

    Create Membership Page

---
+++ membership/index.php
@@ -0,0 +1,26 @@
+<?php require ($_SERVER['DOCUMENT_ROOT'] . "/resources/page/page.php"); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <?php include(INCLUDE_META) ?>
+        
+        <title>TechSoc - Membership</title>
+        
+        <meta name="description" content="What perks you'll get when getting a Technology Society Membership.">
+        
+        <?php include(INCLUDE_STYLE) ?>
+        
+        <style></style>
+        
+        <?php include(INCLUDE_SCRIPTS) ?>
+        
+        <script></script>
+    </head>
+    
+    <body>
+        <?php include(INCLUDE_HEADER) ?>
+        <div class="header-height"></div>
+        <h1 class="title">Membership</h1>
+        <?php include(INCLUDE_FOOTER) ?>
+    </body>
+</html>
