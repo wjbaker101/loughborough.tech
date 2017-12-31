21955c493acb8f0102692f3a2750f88ec4a780c4
wjbaker101 <willbaker10198@gmail.com>
2017-12-31 01:13:23 +0000

    Add Perks Subsection

--- membership/index.php
+++ membership/index.php
@@ -21,6 +21,35 @@
         <?php include(INCLUDE_HEADER) ?>
         <div class="header-height"></div>
         <h1 class="title">Membership</h1>
+        <div class="bordered-section">
+            <div class="content-text-width text-centered padding-small">
+                <h2 class="underlined">Perks</h2>
+                <div class="cell-row">
+                    <div class="cell l4 m12 padding-small">
+                        <div class="card bg-white">
+                            <div class="card-content hpadding-small vpadding-regular">
+                                <svg width="64" height="64" class="cell-middle">
+                                    <use xlink:href="/resources/external/orion/icons.svg#boy-1"></use>
+                                </svg>
+                            </div>
+                            <div class="card-content hpadding-small">
+                                <h3>Stash</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cell l4 m12 padding-small">
+                        <div class="card padding-small bg-white">
+                            b
+                        </div>
+                    </div>
+                    <div class="cell l4 m12 padding-small">
+                        <div class="card padding-small bg-white">
+                            c
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php include(INCLUDE_FOOTER) ?>
     </body>
 </html>
