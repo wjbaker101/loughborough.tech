26da07c1c70eb7e8f164adfc8a7e1ccb4a3f431d
wjbaker101 <willbaker10198@gmail.com>
2018-01-06 14:36:02 +0000

    Remove existing workshop styling and content

--- workshops/index.php
+++ workshops/index.php
@@ -15,42 +15,6 @@
             {
                 height: 150px;
             }
-            
-            .modal
-            {
-                width: 100%;
-                height: 100%;
-                top: 0;
-                left: 0;
-                display: none;
-                position: fixed;
-                background-color: rgba(0, 0, 0, 0.2);
-                z-index: 11;
-            }
-            
-            .modal.is-open
-            {
-                display: table;
-            }
-            
-            .modal.is-open .card
-            {
-                margin-top: 4em;
-                animation: modal-appear 0.5s;
-            }
-            
-            @keyframes modal-appear
-            {
-                from
-                {
-                    transform: translateY(2em);
-                }
-                
-                to
-                {
-                    transform: translateY(0);
-                }
-            }
         </style>
         
         <?php include(INCLUDE_SCRIPTS) ?>
@@ -65,7 +29,6 @@
         <div class="workshops-container content-width hpadding-small vpadding-mid">
             <?php include('resources/get-workshops.php'); ?>
         </div>
-        <?php include('sessions/get-sessions.php'); ?>
         <?php include(INCLUDE_FOOTER) ?>
     </body>
 </html>
