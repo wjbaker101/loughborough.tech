d04c1832e9cbc6de25d14ea4a92f44d0d5d3e63f
wjbaker101 <willbaker10198@gmail.com>
2018-01-27 22:30:42 +0000

    Remove .php extensions

--- code-and-chill/resources/get-sessions.php
+++ code-and-chill/resources/get-sessions.php
@@ -29,10 +29,10 @@ function createSession($title, $date, $beginnersImage, $advancedImage, $beginner
 
 $sessions = array
 (
-    createSession('Session One', '2017-10-11 16:00:00', 'html5.svg', 'sass.svg', 'HTML and CSS<br><small>Part 1</small>', 'CSS and SASS', 'session/11-10-2017.php'),
-    createSession('Session Two', '2017-10-25 16:00:00', 'css3.svg', 'nodejs.png', 'HTML and CSS<br><small>Part 2</small>', 'Node.js', 'session/25-10-2017.php'),
-    createSession('Session Three', '2017-11-08 16:00:00', 'javascript.jpg', 'angularjs.svg', 'HTML and CSS<br><small>Part 3</small>', 'AngularJS', 'session/08-11-2017.php'),
-    createSession('Session Four', '2017-12-13 16:00:00', 'html5.svg', 'python.svg', 'Coursework Help and JavaScript', 'Python', 'session/13-12-2017.php')
+    createSession('Session One', '2017-10-11 16:00:00', 'html5.svg', 'sass.svg', 'HTML and CSS<br><small>Part 1</small>', 'CSS and SASS', 'session/11-10-2017/'),
+    createSession('Session Two', '2017-10-25 16:00:00', 'css3.svg', 'nodejs.png', 'HTML and CSS<br><small>Part 2</small>', 'Node.js', 'session/25-10-2017/'),
+    createSession('Session Three', '2017-11-08 16:00:00', 'javascript.jpg', 'angularjs.svg', 'HTML and CSS<br><small>Part 3</small>', 'AngularJS', 'session/08-11-2017/'),
+    createSession('Session Four', '2017-12-13 16:00:00', 'html5.svg', 'python.svg', 'Coursework Help and JavaScript', 'Python', 'session/13-12-2017/')
 );
 
 echo '<div class="column-container">';
